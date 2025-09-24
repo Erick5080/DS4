392440a2d141183f4d304fb1832739c6accafca2
Erick5058 <erick.ospina@utp.ac.pa>
2025-09-24 07:18:39 -0500

    Laboratorio 6.1

---
+++ laboratorio 5/laboratorio 5.6/laboratorio 5.6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+        {
+            { "Francia", "París" },
+            { "España", "Madrid" },
+            { "Italia", "Roma" }
+        };
+
+        foreach (KeyValuePair<string, string> par in paisesYCapitales)
+        {
+            Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+        }
+    }
+}
