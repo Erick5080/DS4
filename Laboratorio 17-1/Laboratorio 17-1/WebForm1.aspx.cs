000a4512bc38af757473f903096e13d54f83881c
Erick <erick.ospina@utp.ac.pa>
2025-12-06 15:45:13 -0500

    Agregando el lab 18

--- Laboratorio 17-1/Laboratorio 17-1/WebForm1.aspx.cs
+++ Laboratorio 17-1/Laboratorio 17-1/WebForm1.aspx.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 namespace Laboratorio_17_1
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["Conexiónnorthwind"];
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+                conexion.Open();
 
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        if (reader.Read())
+                        {
+                            GridV.DataSource = reader;
+                            GridV.DataBind();
+                        }
+                    }
+                }
+            }
         }
     }
 }
